1edc28ae11c58c3c4b3d7c9859eba42a65a25583
Fredrik Berglund <fredrik@diam.se>
2024-01-12 15:34:20 +0100

    Starting classes

--- index.js
+++ index.js
@@ -375,3 +375,38 @@ console.log(
 
 console.log("Task 10:", numbers);
 */
+
+class School {
+	constructor(name, adress, zipcode, city) {
+		this.name = name;
+		this.adress = adress;
+		this.zipcode = zipcode;
+		this.city = city;
+		this.students = [];
+		this.teachers = [];
+	}
+}
+
+class Subject {
+	constructor(name) {
+		this.name = name;
+		this.students = [];
+		this.teachers = [];
+	}
+}
+
+class Students {
+	constructor(name, age, gender) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.subjects = [];
+	}
+}
+
+class Teachers {
+	constructor(name) {
+		this.name = name;
+		this.subjects = [];
+	}
+}
