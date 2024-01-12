baa85e63ecb71b86f59160acd67e8da18fc9caf8
Fredrik Berglund <fredrik@diam.se>
2024-01-12 14:03:09 +0100

    Update

--- dom.js
+++ dom.js
@@ -12,6 +12,24 @@ const imageLink = imageElement.src;
 const imageHeight = imageElement.height;
 const imageWidth = imageElement.width;
 
+const ingredientHTML = document.querySelector(".ingredients-list-paste");
+const ingredientsLi = ingredientHTML.querySelectorAll("li");
+const ingredients4 = ingredientsLi[4].textContent;
+const ingredientsNum = ingredientsLi.length;
+
+const instructionHTML = document
+	.querySelector(".instructions-list")
+	.querySelectorAll("li");
+
+var instructions = [];
+let insCount = 0;
+for (let i = 0; i < instructionHTML.length; i++) {
+	insCount++;
+	let text = instructionHTML[i].textContent.trim().replace(/\s+/g, " ");
+
+	instructions.push({ order: insCount, text: text });
+}
+
 let imageObject = {
 	url: imageLink,
 	height: imageHeight,
@@ -23,4 +41,9 @@ console.log("The name of the HTML ID is:", repNameId);
 console.log("Font size:", fontSize);
 console.log("Image Alt:", imageAlt);
 
-console.log(imageObject);
+console.log("Image objects:", imageObject);
+
+console.log("The paste has " + ingredientsNum + " ingredients.");
+console.log("The 4th ingredient is", ingredients4);
+
+console.log("Instructions Array Object:", instructions);
